4eba48b327d598b1974af79fdb18efcf2df4aaa6
Michael Anderson <52722296+and02709@users.noreply.github.com>
2023-09-04 10:09:29 -0500

    Update spls.r

--- R/spls.r
+++ R/spls.r
@@ -15,13 +15,13 @@ spls.r <- function(X,Y,ncomp,mode=NULL,lam1=NULL,lam2=NULL,trace=F,niter=200,thr
   U <- matrix(0, nrow=ncol(X), ncol=ncomp)
   V <- matrix(0, nrow=ncol(Y), ncol=ncomp)
   for(i in 1:ncomp){
-    uv.temp <- plss::pls.svd(Xi,Yi,1)
+    uv.temp <- PLSS::pls.svd(Xi,Yi,1)
     u <- uv.temp$u
     v <- uv.temp$v
     if(!is.null(lam1) || !is.null(lam2)){
       if(is.null(lam1)) lam1 <- 0
       if(is.null(lam2)) lam2 <- 0
-      uv.temp <- plss::ssvd(M=uv.temp$M,u=u,v=v,lam1=lam1,lam2=lam2,trace=trace,niter=niter,threshold=threshold,ncomp=i)
+      uv.temp <- PLSS::ssvd(M=uv.temp$M,u=u,v=v,lam1=lam1,lam2=lam2,trace=trace,niter=niter,threshold=threshold,ncomp=i)
       u <- uv.temp$u
       v <- uv.temp$v
     }
@@ -33,7 +33,7 @@ spls.r <- function(X,Y,ncomp,mode=NULL,lam1=NULL,lam2=NULL,trace=F,niter=200,thr
     U[,i] <- u
     V[,i] <- v
     if(i==ncomp) break
-    xy.temp <- plss::pls.deflate(X=Xi,Y=Yi,u=u,v=v,mode)
+    xy.temp <- PLSS::pls.deflate(X=Xi,Y=Yi,u=u,v=v,mode)
     Xi <- xy.temp$Xi
     Yi <- xy.temp$Yi
   }
